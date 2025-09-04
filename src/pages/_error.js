7b82672e004a49f69c73a4668e5b8a141c9e6f95
itanne99 <idovader@gmail.com>
2025-09-04 12:57:51 -0400

    Added '404' support

---
+++ src/pages/_error.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import React, { useEffect } from 'react'
+import { Container } from 'react-bootstrap'
+
+function _error(props) {
+
+  useEffect(() => {
+    console.log(`props:`, props);
+  }, [])
+
+  return (
+    <Container fluid className="d-flex flex-grow-1 align-items-center justify-content-center">
+      <div className="text-center">
+        <h1 className="display-1 fw-bold">404</h1>
+        <p className="fs-3"> <span className="text-danger">Opps!</span> Page not found.</p>
+        <p className="lead">
+          The page you’re looking for doesn’t exist.
+        </p>
+        <Link href="/" className="btn btn-primary">Go Home</Link>
+      </div>
+    </Container>
+  )
+}
+
+export default _error
