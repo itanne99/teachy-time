c030b9c2c9dbe1cbb9f0df7db6e2baeaa62a34f0
itanne99 <idovader@gmail.com>
2025-09-04 16:35:32 -0400

    Adjusted error/404 support

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import React, { useEffect } from 'react'
 import { Container } from 'react-bootstrap'
 
-function _error(props) {
+function Custom404(props) {
 
   useEffect(() => {
     console.log(`props:`, props);
@@ -12,9 +12,9 @@ function _error(props) {
     <Container fluid className="d-flex flex-grow-1 align-items-center justify-content-center">
       <div className="text-center">
         <h1 className="display-1 fw-bold">404</h1>
-        <p className="fs-3"> <span className="text-danger">Opps!</span> An error occurred.</p>
+        <p className="fs-3"> <span className="text-danger">Opps!</span> Page not found.</p>
         <p className="lead">
-          Error Message
+          The page you’re looking for doesn’t exist.
         </p>
         <Link href="/" className="btn btn-primary">Go Home</Link>
       </div>
@@ -22,4 +22,4 @@ function _error(props) {
   )
 }
 
-export default _error
+export default Custom404
